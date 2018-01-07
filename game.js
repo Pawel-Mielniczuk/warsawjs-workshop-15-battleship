84c17296f2ab86688081e4b0e2305143a52304ba
Brox <mrjeronimo@onet.pl>
2018-01-07 12:40:08 +0100

    Add clicable event

--- game.js
+++ game.js
@@ -1,12 +1,34 @@
-const gameContainer = document.querySelector("#game");
-const cell = document.createElement("div");
-const textNode = document.createTextNode("Hell, world");
-//cell.textContent = "Hello!";
-gameContainer.appendChild(cell);
+'use strict';
 
+// klasa widoku
 
-let onClick = function onClick() {
-  cell.textContent = "Clicked";
-}; 
+class Component {
+  getElement() {
+    return this._element;
+  }
+}
 
-cell.addEventListener("click", onClick);
+class cellComponent extends Component {
+  constructor() {
+    super();
+    this._state = "unknown";
+    this._element = document.createElement("td");
+    this._element.addEventListener("click",(function(){
+      this.setState("miss");
+    }).bind(this));
+    this._refresh();
+  }
+
+setState(stateName) {
+  this._state = stateName;
+  this._refresh();
+}
+
+_refresh() {
+  this._element.textContent = this._state;
+  this._element.className = "cell_" + this._state;
+}
+}
+
+const myCell = new cellComponent();
+document.getElementById("cellContainer").appendChild(myCell.getElement());
